e958d9764a3ac5d3dc62422a90d67460ae32a8c9
Ze40 <miko.r4r@gmail.com>
2024-12-23 18:51:03 +0300

    featch(combLib): Добавил нахождение количества сочитаний

--- Program.cs
+++ Program.cs
@@ -23,6 +23,11 @@
                     case "4":
                         Task4 task4 = new Task4();
                         break;
+                    case "5":
+                        break;
+                    case "6":
+                        Task6 task6 = new Task6();
+                        break;
                     default:
                         Console.WriteLine("Нет такого задания");
                         break;
---
+++ Task6.cs
@@ -0,0 +1,18 @@
+﻿using CombLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iz11
+{
+    internal class Task6
+    {
+        public Task6() {
+            Comb comb = new Comb();
+            long c = comb.CountOfCombinations(10, 2);
+            Console.WriteLine(c);
+        }
+    }
+}
